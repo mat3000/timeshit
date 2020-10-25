fcb0f6d2696d38326d53565f8f8ba7c6b5afb68d
mathieu <matcorbeille@gmail.com>
2020-10-25 22:51:13 +0100

    env

--- src/overmind/services.js
+++ src/overmind/services.js
@@ -3,9 +3,9 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC9IZpvqiiCDgHyj-iByJ0WpyGuYDJ1NrY',
-  authDomain: 'test-38810.firebaseapp.com',
-  databaseURL: 'https://test-38810.firebaseio.com',
+  apiKey: process.env.REACT_APP_FB_APIKEY,
+  authDomain: `${process.env.REACT_APP_FB_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.REACT_APP_FB_ID}.firebaseio.com`,
 };
 
 firebase.initializeApp(firebaseConfig);
